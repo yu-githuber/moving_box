08ed084cb547b534f4291a9dff00da131ac8ffad
yuzhanhai <yuzhanhai18@163.com>
2019-04-10 16:46:10 +0800

    detect six plane

---
+++ readme.txt
@@ -0,0 +1,8 @@
+You can start the project by using the following three commands
+
+"catkin_make"
+"source devel/setup.bash"
+"roslaunch box_detector box_detector.launch"
+
+And the detected plane (named plane_*.pcd) is stored in the folder "/home/username/.ros/"
+
--- src/box_detector/nodes/box_subscriber/box_subscriber_node.cpp
+++ src/box_detector/nodes/box_subscriber/box_subscriber_node.cpp
@@ -33,11 +33,16 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
 
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/extract_indices.h>
+
 static pcl::PointCloud<pcl::PointXYZ>::Ptr plane_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-static pcl::PointCloud<pcl::PointXYZ>::Ptr box_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-static pcl::PointCloud<pcl::PointXYZ>::Ptr filter_box_cloud(new pcl::PointCloud<pcl::PointXYZ>());
 static pcl::PointCloud<pcl::PointXYZ>::Ptr whole_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-static pcl::PointCloud<pcl::PointXYZ>::Ptr transform_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+static pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f(new pcl::PointCloud<pcl::PointXYZ>());
 
 static sensor_msgs::PointCloud2 cloud_msg;
 
@@ -47,7 +52,81 @@ using namespace std;
 
 void cloud_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
 {
-    return ;
+    // Read in the cloud data
+    pcl::fromROSMsg(*input,*whole_cloud);
+ 
+	// Create the segmentation object for the planar model and set all the parameters
+	pcl::SACSegmentation<pcl::PointXYZ> seg;
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PCDWriter writer;
+	seg.setOptimizeCoefficients(true);
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setMethodType(pcl::SAC_RANSAC);
+	seg.setMaxIterations(300);
+	seg.setDistanceThreshold(0.05);
+    
+    int i=0;
+	while (whole_cloud->points.size() > 0)
+	{
+		// Segment the largest planar component from the remaining cloud
+		seg.setInputCloud(whole_cloud);
+		seg.segment(*inliers, *coefficients);
+		if (inliers->indices.size() == 0)
+		{
+			std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+			break;
+		}
+		
+		char filepath2[100];
+		sprintf(filepath2, "plane_%d.pcd", i);
+		
+ 
+		// Extract the planar inliers from the input cloud
+		pcl::ExtractIndices<pcl::PointXYZ> extract;
+		extract.setInputCloud(whole_cloud);
+		extract.setIndices(inliers);
+ 
+ 
+		// Get the points associated with the planar surface
+		extract.filter(*cloud_plane);
+ 
+		if (cloud_plane->points.size()>0)
+		{
+			std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size() << " data points." << std::endl;
+ 
+			pcl::PointCloud<pcl::PointXYZRGB>::Ptr colored_cloud;
+			colored_cloud = (new pcl::PointCloud<pcl::PointXYZRGB>)->makeShared();
+			std::vector<unsigned int> colors;
+			colors.push_back(static_cast<unsigned int> (rand() % 256));
+			colors.push_back(static_cast<unsigned int> (rand() % 256));
+			colors.push_back(static_cast<unsigned int> (rand() % 256));
+ 
+			colored_cloud->width = cloud_plane->width;
+			colored_cloud->height = cloud_plane->height;
+ 
+			colored_cloud->is_dense = cloud_plane->is_dense;
+			for (size_t i_point = 0; i_point < cloud_plane->points.size(); i_point++)
+			{
+				pcl::PointXYZRGB point;
+				point.x = *(cloud_plane->points[i_point].data);
+				point.y = *(cloud_plane->points[i_point].data + 1);
+				point.z = *(cloud_plane->points[i_point].data + 2);
+				point.r = colors[0];
+				point.g = colors[1];
+				point.b = colors[2];
+				colored_cloud->points.push_back(point);
+			}
+			pcl::io::savePCDFileASCII(filepath2, *colored_cloud);
+			++i;
+		}
+		// Remove the planar inliers, extract the rest
+		extract.setNegative(true);
+		extract.filter(*cloud_f);
+		*whole_cloud = *cloud_f;
+	}
+ 
 }
 
 int main(int argc, char **argv)
@@ -61,3 +140,5 @@ int main(int argc, char **argv)
     
     return 0;
 }
+
+	
